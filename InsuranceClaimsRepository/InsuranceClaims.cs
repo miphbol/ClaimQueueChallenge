d66b5199ee11ba4cb868631deabf5d64f40eac82
Mitch Boling <mphboling@gmail.com>
2024-05-09 18:59:58 -0400

    Initial commit

---
+++ InsuranceClaimsRepository/InsuranceClaims.cs
@@ -0,0 +1,25 @@
+﻿namespace InsuranceClaims.Repository;
+
+public class InsuranceClaims
+{
+    public int ClaimID { get; set; }
+    public string ClaimType { get; set; }
+    public string ClaimDescription { get; set; }
+    public double ClaimAmount { get; set; }
+    public int DateOfIncident { get; set; }
+    public int DateOfClaim { get; set; }
+    public bool IsValid { get; set; }
+
+    public InsuranceClaims() {}
+
+    public InsuranceClaims(int claimid, string claimtype, string claimdescription, double claimamount, int dateofincident, int dateofclaim, bool isvalid)
+    {
+        ClaimID = claimid;
+        ClaimType = claimtype;
+        ClaimDescription = claimdescription;
+        ClaimAmount = claimamount;
+        DateOfIncident = dateofincident;
+        DateOfClaim = dateofclaim;
+        IsValid = isvalid;
+    }
+}
